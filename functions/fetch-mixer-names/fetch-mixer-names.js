5ca49f78481c6e00c2d7847c5158300c4d74a184
Ryan Harris <harrisryan1@gmail.com>
2019-12-02 19:46:11 -0500

    Add catch-mixer-event and fetch-mixer-names functions

---
+++ functions/fetch-mixer-names/fetch-mixer-names.js
@@ -0,0 +1,36 @@
+const fetch = require('node-fetch');
+
+exports.handler = async (event, context) => {
+  try {
+    const streamerMarkdown = await fetch(
+      'https://raw.githubusercontent.com/ChristopherBiscardi/party-corgi/master/docs/list-of-streamers.md'
+    )
+      .then(res => res.text())
+      .then(body => {
+        return body;
+      });
+
+    const mixerRegex = new RegExp(
+      /(https?:\/\/(.+?\.)?mixer\.com(\/[A-Za-z0-9\-\._~:\/\?#\[\]@!$&'\(\)\*\+,;\=]*)?)/g
+    );
+    const mixerUrls = Array.from(streamerMarkdown.match(mixerRegex));
+
+    const twitchRegex = new RegExp(
+      /(https?:\/\/(.+?\.)?twitch\.tv(\/[A-Za-z0-9\-\._~:\/\?#\[\]@!$&'\(\)\*\+,;\=]*)?)/g
+    );
+    const twitchUrls = Array.from(streamerMarkdown.match(twitchRegex));
+
+    const response = {
+      mixer: mixerUrls,
+      twitch: twitchUrls
+    };
+    console.log(response);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response)
+    };
+  } catch (err) {
+    return { statusCode: 500, body: `ERROR: ${err.toString()}` };
+  }
+};
